6d6118bc3cdc16abe77714c8a3ecd948bf1969df
Iman Anooshehpour <imananoosheh@gmail.com>
2022-12-27 15:42:08 -0800

    Rebased with image and overlay approach

--- src/App.tsx
+++ src/App.tsx
@@ -3,69 +3,79 @@ import reactLogo from "./assets/react.svg";
 import bgIMG from "./assets/img.jpg";
 import "./App.css";
 import LegoCanvas from "./components/LegoCanvas";
+import { set } from "zod";
 
 function App() {
-    const CANVAS_SIZE = 1000
-    const [count, setCount] = useState(0);
-
-    function draw(canvasContext: CanvasRenderingContext2D) {
-        const backgroundImage = new Image();
-        backgroundImage.src = bgIMG
-        const backgroundImageWidth = backgroundImage.width;
-        const backgroundImageHeight = backgroundImage.height;
-        canvasContext?.drawImage(
-            backgroundImage,
-            0,
-            0,
-            backgroundImageWidth,
-            backgroundImageHeight
-        );
-    }
+    const imgWidth = 1350;
+    const imgHeight = 1080;
+    const [blurValue, setBlurValue] = useState('8');
+    const [sizeValue, setSizeValue] = useState('50');
 
     return (
         <div className="App">
-            <div id="main-container">
-                <div id="upload-section" className="container-cell">
-                    <button data-upload-button>Upload</button>
+            <div
+                id="main-container"
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-around"
+                }}
+            >
+                <div
+                    style={{
+                        maxWidth: "500px",
+                    }}
+                >
+                    <img src="./src/assets/img.jpg" alt="by sabet" style={{maxWidth: "100%"}}/>
+                    <div id="lego-overlay-container"></div>
                 </div>
-                <div id="canvas-container" className="container-cell">
-                    <LegoCanvas draw={draw} height={CANVAS_SIZE} width={CANVAS_SIZE} />
-                </div>
-                <div id="controller-output" className="container-cell">
-                    <div className="controller-container">
-                        <label htmlFor="lego-size">Lego Brick Size</label>
+                <div style={{ maxWidth: "500px" }}>
+                    <div id="blur-volume-container">
+                        <label htmlFor="volume">Blur Volume</label>
                         <input
                             type="range"
-                            id="lego-size"
-                            name="lego-size"
-                            min="20"
-                            max="80"
-                            list="sizemarks"
-                        ></input>
-                        <datalist id="sizemarks">
-                            <option value="20" label="20"></option>
-                            <option value="40" label="40"></option>
-                            <option value="80" label="80"></option>
-                        </datalist>
-                    </div>
-                    <div className="controller-container">
-                        <label htmlFor="blur-volume">Lego Blur Volume</label>
-                        <input
-                            type="range"
+                            value={blurValue}
                             id="blur-volume"
-                            name="blur-volume"
-                            min="1"
-                            max="120"
-                            list="blurmarks"
+                            name="volume"
+                            min={'0.1'}
+                            max={'100'}
+                            onChange={(event)=>{setBlurValue(event.target.value)}}
                         ></input>
-                        <datalist id="blurmarks">
-                            <option value="0" label="0"></option>
-                            <option value="60" label="60"></option>
-                            <option value="120" label="120"></option>
-                        </datalist>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                fontSize: "1rem",
+                                color: "white",
+                            }}
+                        >
+                            <span>0.1</span>
+                            <span>100</span>
+                        </div>
                     </div>
-
-                    <button data-download-button>Download</button>
+                    <div id="size-volume-container">
+                        <label htmlFor="lego-size">Lego Size Volume ({sizeValue}) </label>
+                        <input
+                            type="range"
+                            value={sizeValue}
+                            id="lego-size-volume"
+                            name="lego-size"
+                            min={'20'}
+                            max={'200'}
+                            onChange={(event)=> {setSizeValue(event.target.value)}}
+                        ></input>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                fontSize: "1rem",
+                                color: "white",
+                            }}
+                        >
+                            <span>20</span>
+                            <span>200</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
