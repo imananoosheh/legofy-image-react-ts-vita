d99fc1d717b3cd4e1818f78299ea52c302370c09
Iman Anooshehpour <imananoosheh@gmail.com>
2023-01-05 22:39:39 -0800

    tweaked controller styles

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,10 @@ function App() {
         const gridNumOfRow = Math.ceil(imgHeight / Number(sizeValue));
         const numberOfLego = gridNumOfColumn * gridNumOfRow;
 
-        const legoAndImageMarginWidth = imgWidth - (gridNumOfColumn*Number(sizeValue))
-        const legoAndImageMarginHeight = imgHeight - (gridNumOfRow*Number(sizeValue))
+        const legoAndImageMarginWidth =
+            imgWidth - gridNumOfColumn * Number(sizeValue);
+        const legoAndImageMarginHeight =
+            imgHeight - gridNumOfRow * Number(sizeValue);
         // legoOverlayContainer?.setAttribute("style", `inset: ${legoAndImageMarginHeight} ${legoAndImageMarginWidth}`)
         // console.log(`inset: ${legoAndImageMarginHeight} ${legoAndImageMarginWidth}`)
 
@@ -60,7 +62,9 @@ function App() {
             brickList.push(legoBrick);
         }
         if (legoOverlayContainer) {
-            const newStyle = `grid-template-columns:repeat(${gridNumOfColumn}, ${sizeValue}px);grid-template-rows:repeat(${gridNumOfRow}, ${sizeValue}px);inset: ${legoAndImageMarginHeight/2}px ${legoAndImageMarginWidth/2}px`;
+            const newStyle = `grid-template-columns:repeat(${gridNumOfColumn}, ${sizeValue}px);grid-template-rows:repeat(${gridNumOfRow}, ${sizeValue}px);inset: ${
+                legoAndImageMarginHeight / 2
+            }px ${legoAndImageMarginWidth / 2}px`;
             legoOverlayContainer.setAttribute("style", newStyle);
             // legoOverlayContainer.style['grid-template-columns'] = `repeat(${gridNumOfColumn}, ${sizeValue}px)`
             // legoOverlayContainer.style['grid-template-rows'] = `repeat(${gridNumOfRow}, ${sizeValue}px)`
@@ -95,9 +99,12 @@ function App() {
         const legoBricks = document
             .querySelectorAll(".lego-brick")
             .forEach((brick) => {
-                brick.setAttribute('style',`width:${sizeValue}px;height:${sizeValue}px`)
+                brick.setAttribute(
+                    "style",
+                    `width:${sizeValue}px;height:${sizeValue}px`
+                );
                 //TODO: continue from here
-                // brick.firstChild. 
+                // brick.firstChild.
                 // setAttribute('style',`width:${sizeValue}px;height:${sizeValue}px`)
             });
     }, [sizeValue]);
@@ -131,7 +138,15 @@ function App() {
                     <img src={bgImage} data-uploaded-image alt="" />
                     <div id="lego-overlay-container"></div>
                 </section>
-                <div style={{ maxWidth: "500px" }}>
+                <div
+                    style={{
+                        height: "max(26vh, 20vw)",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
                     <div id="image-input-container">
                         <input
                             type="file"
@@ -140,10 +155,11 @@ function App() {
                             onChange={handleUpload}
                         />
                     </div>
-                    <div id="blur-volume-container">
+                    <div id="blur-volume-container" style={{width: "100%"}}>
                         <label htmlFor="volume">
                             Blur Volume ({blurValue})
-                        </label><br></br>
+                        </label>
+                        <br></br>
                         <input
                             type="range"
                             value={blurValue}
@@ -168,10 +184,11 @@ function App() {
                             <span>100</span>
                         </div>
                     </div>
-                    <div id="size-volume-container">
+                    <div id="size-volume-container" style={{width: "100%"}}>
                         <label htmlFor="lego-size">
                             Lego Size Volume ({sizeValue}){" "}
-                        </label><br></br>
+                        </label>
+                        <br></br>
                         <input
                             type="range"
                             value={sizeValue}
